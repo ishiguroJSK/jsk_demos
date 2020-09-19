494d834868d1e78084ba1c1a7912b74e0667c8db
ishiguroJSK <ishiguro@jsk.imi.i.u-tokyo.ac.jp>
2020-09-19 22:21:14 +0900

    up

--- teleop_dual_arm/PR2ScreenPoint2DualArmPose2.py
+++ teleop_dual_arm/PR2ScreenPoint2DualArmPose2.py
@@ -3,6 +3,7 @@
 import rospy
 import tf
 import copy
+import math
 from geometry_msgs.msg import *
 from pr2_controllers_msgs.msg import Pr2GripperCommandActionGoal
 
@@ -18,17 +19,17 @@ clicked_pos = [PoseStamped() for lr in LR]
 def click_cb(msg):
   print msg
   stamp = rospy.Time.now()
-    try:
-      tfl.waitForTransform("/base_link", "/ray_target", stamp, timeout=rospy.Duration(1))
-      (pos, rot) = tfl.lookupTransform("/base_link", "/ray_target", rospy.Time(0))
-    except Exception, e:
-      rospy.logerr(e)
-      continue
+  try:
+    tfl.waitForTransform("/base_link", "/ray_target", stamp, timeout=rospy.Duration(1))
+    (pos, rot) = tfl.lookupTransform("/base_link", "/ray_target", rospy.Time(0))
+  except Exception, e:
+    rospy.logerr(e)
+    return
 
-  tgtid = 0 if (pos[1]>0) else 1 ## divide center pose into L R
+  tgtid = 0 if (pos[1]>0.0) else 1 ## divide center pose into L R
   clicked_pos[tgtid].header.frame_id = "base_link"
   clicked_pos[tgtid].header.stamp = stamp
-  clicked_pos[tgtid].pose.position.x = pos[0] + 0.02
+  clicked_pos[tgtid].pose.position.x = pos[0]
   clicked_pos[tgtid].pose.position.y = pos[1]
   clicked_pos[tgtid].pose.position.z = pos[2]
   clicked_pos[tgtid].pose.orientation.x = 0
@@ -48,33 +49,38 @@ if __name__ == '__main__':
   tfl = tf.TransformListener()
   print "start ROS pub loop"
   HZ = 10
-  r = rospy.Rate(HZ)
 
   # init
-  for s, v in zip(sgns, vals):
-    v.header.frame_id = "base_link"
-    v.pose.position.x = 0.5
-    v.pose.position.y = s * 0.3
-    v.pose.position.z = 1.0
-    v.pose.orientation.x = 0
-    v.pose.orientation.y = 0
-    v.pose.orientation.z = 0
-    v.pose.orientation.w = 1
+  for i in range(len(lr)):
+    vals[i].header.frame_id = "base_link"
+    vals[i].pose.position.x = 0.5
+    vals[i].pose.position.y = sgns[i] * 0.3
+    vals[i].pose.position.z = 1.0
+    vals[i].pose.orientation.x = 0
+    vals[i].pose.orientation.y = 0
+    vals[i].pose.orientation.z = 0
+    vals[i].pose.orientation.w = 1
   
   
+  t_after_click = [0,0]
   while not rospy.is_shutdown():
-    t_after_click = (rospy.Time.now() - clicked_pos[0].header.stamp).to_sec()
-    if t_after_click > 3:
-      t = t_after_click - 3
-      theta = math.pi * (t / 3.0)
-      theta = min(max(0,theta),90) # 0~90
-      r = 0.1
-      vals = copy.deepcopy(clicked_pos[0])
-      vals.pose.position.x -= r * math.sin(theta)
-      vals.pose.position.z -= r * (1-math.cos(theta))
-
-    else:
-      vals = copy.deepcopy(clicked_pos[0])
+    rospy.loginfo("sadf")
+    for p, v, cp, lr in zip(pubs, vals, clicked_pos, LR):
+      rospy.loginfo(cp.header.stamp)
+      t_after_click = (rospy.Time.now() - cp.header.stamp).to_sec()
+      if t_after_click > 9:
+        t = t_after_click - 9
+        theta = math.pi * (t / 3.0)
+        theta = min(max(math.radians(0),theta),math.radians(90)) # 0~90
+        r = 0.2
+        v = copy.deepcopy(cp)
+        v.pose.position.x -= r * math.sin(theta)
+        v.pose.position.z -= r * (1-math.cos(theta))
+      else:
+        v = copy.deepcopy(cp)
+      
+      if lr == "l" :
+        p.publish(v)
 
     
 
@@ -92,28 +98,28 @@ if __name__ == '__main__':
 
 
     
-    for i, (v, v_not_move, p, gp) in enumerate(zip(vals, vals_not_move, pubs, g_pubs)):
-      if v_not_move < 1.5*HZ or 4*HZ < v_not_move :
-        # hover
-        v_mod = copy.deepcopy(v)
-        v_mod.pose.position.z += 0.2
-        p.publish(v_mod)
+    # for i, (v, v_not_move, p, gp) in enumerate(zip(vals, vals_not_move, pubs, g_pubs)):
+    #   if v_not_move < 1.5*HZ or 4*HZ < v_not_move :
+    #     # hover
+    #     v_mod = copy.deepcopy(v)
+    #     v_mod.pose.position.z += 0.2
+    #     p.publish(v_mod)
         
-        if g_flags[i] == True and g_vals[i].goal.command.position == 0:
-          g_flags[i] = False
+    #     if g_flags[i] == True and g_vals[i].goal.command.position == 0:
+    #       g_flags[i] = False
           
-        if g_flags[i] == False and g_vals[i].goal.command.position != 0:
-          g_flags[i] = True
+    #     if g_flags[i] == False and g_vals[i].goal.command.position != 0:
+    #       g_flags[i] = True
           
-      else :
-        # touch
-        p.publish(v)
+    #   else :
+    #     # touch
+    #     p.publish(v)
         
-        if 2*HZ < v_not_move:
-          g_vals[i].goal.command.position = ( 0 if g_flags[i] else 0.1)
-          g_vals[i].goal.command.max_effort = 25;
-          gp.publish(g_vals[i])
+    #     if 2*HZ < v_not_move:
+    #       g_vals[i].goal.command.position = ( 0 if g_flags[i] else 0.1)
+    #       g_vals[i].goal.command.max_effort = 25;
+    #       gp.publish(g_vals[i])
 
 
 
-    r.sleep()
+    rospy.Rate(HZ).sleep()
